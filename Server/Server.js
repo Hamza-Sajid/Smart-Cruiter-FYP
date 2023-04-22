c8f46912dd413c2541d7bc8cf8e1e33761ccdb7c
Hamza-Sajid <pak786hamzasajid66@gmail.com>
2023-04-22 21:46:45 +0500

    deployment fixing

--- Server/Server.js
+++ Server/Server.js
@@ -38,6 +38,7 @@ app.use("/", (req, res, next) => {
 // app.use('/details', RecruitmentRouter)
 //app listening
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+
+app.listen(3232, () => {
     console.log("server is running on port :" + port)
 })
