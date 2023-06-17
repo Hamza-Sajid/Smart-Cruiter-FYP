ea3f6a3120b744a69b6d206acf079d3e69a64cfb
Hamza-Sajid <pak786hamzasajid66@gmail.com>
2023-06-17 10:55:22 +0500

    integrated 404 page

--- Client/src/Pages/Dashboard/NotPageFound404.jsx
+++ Client/src/Pages/Dashboard/NotPageFound404.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import NotFound from "../../assets/illustrations/404.png";
 function NotPageFound404() {
   return (
-    <div className="p-6 bg-gray-600 ">
-      <h2 className="heading2 text-white">NotPageFound404</h2>
+    <div className="p-6 bg-gray-700 h-screen flex-col flex justify-center items-center ">
+      <div>
+        <img className="w-3/4 sm:w-2/6 block m-auto" src={NotFound}></img>
+      </div>
+      <div>
+        <h2 className="heading2 mt-8 text-white">Enter valid address!</h2>
+      </div>
     </div>
   );
 }
